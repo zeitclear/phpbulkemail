65ada4763f21c0bebf75ed34d307a0e0f5a90a70
Mateus Zeit <zeit4social@gmail.com>
2020-08-23 18:31:24 -0300

    Spam STYLEEE

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# phpbulkemail
-Simple script to send massive emails.
+# Welcome to phpbulkemail!
+
+**Hi! This script is created in PHP for send massive emails of Gmail to Gmail.**
+
+![enter image description here](https://i.imgur.com/GBCGrWg.png)
+
+# How use?
+<h3>don't forget to enable access to <a href="https://myaccount.google.com/lesssecureapps">less secure apps</a> in your Google account. Also, you need to install the <a href="https://getcomposer.org/">composer.</a></h3><br><br>
+
+
+**Follow the tutorial:**
+
+Open the terminal.
+
+    git clone https://github.com/zeitclear/phpbulkemail
+ 
+Find the composer.js file and..
+
+    composer install
+
+Before,
+
+    php main.php
+
+and enjoy.
+
+
---
+++ main.php
@@ -0,0 +1,68 @@
+<?php 
+
+function ask_hidden() {
+	echo "[#] Your password: ";
+
+	echo "\033[30;40m";  
+	$password = readline();
+	echo "\033[0m";      
+
+	return rtrim($password, "\n");
+}
+
+echo "\n-> phpbulkemail\n";
+echo "-> Send massive emails (GMAIL TO GMAIL).\n";
+echo "-> ACTIVE access for less secure apps in Gmail!\n";
+echo "------------------------------------------------ \n\n";
+
+$email = readline("[#] Your email: ");
+
+$password = ask_hidden();
+
+$emailToAttack = readline("[#] Email to attack: ");
+$quantity = readline("[#] Quantity to send emails: ");
+
+echo "------------------------------------------------ \n";
+
+$subject = readline("[!] Subject: ");
+$body = readline("[!] Body: ");
+
+echo "------------------------------------------------ \n";
+
+
+// PHPMailer required (composer require phpmailer/phpmailer)
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+$mail = new PHPMailer(true);
+
+
+$count = 0;
+while($count <= $quantity) {
+    try {     
+        $mail->isSMTP();                                            
+        $mail->Host       = 'smtp.gmail.com';                 
+        $mail->SMTPAuth   = true;                                  
+        $mail->Username   = $email;                    
+        $mail->Password   = $password;                            
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         
+        $mail->Port       = 587;                                   
+    
+        $mail->setFrom($email);
+        $mail->addAddress($emailToAttack);    
+    
+        $mail->isHTML(true);                                  
+        $mail->Subject = $subject;
+        $mail->Body    = $body;
+    
+        $mail->send();
+
+        echo "[$count] Email sent to {$emailToAttack}!\n";
+    } catch (Exception $e) {
+        echo "Error: {$mail->ErrorInfo}";
+    }
+    $count++;
+}
+?>
